564a44146ae22eea71f73c6e83c6022883d80212
Amaan Ahmed <amaan.ahmed3@gmail.com>
2018-10-31 11:31:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function sayHiToGrandma(string) {
     return `YES INDEED!`
   } else if (string === "I love you, Grandma.") {
     return `I love you, too.`
-  } else if (string.toLowerCase ==== string) {
+  } else if (string.toLowerCase === string) {
     return `I can't hear you!`
   } else {
     return `Say again?`
